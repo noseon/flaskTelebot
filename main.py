d14b9d6a5ce3819855ba00e8492169bedce4391e
noseon <fake.on@gmail.com>
2023-07-08 13:57:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
-import telegram, os
+import os
 from flask import Flask, request
-from telegram.ext import Dispatcher, MessageHandler, Filters
-
+import telebot
 
 
 #################
@@ -13,51 +11,29 @@ import openai
 	
 openai.api_key = os.getenv("OPENAI_API_KEY") 
 
-chat_language = os.getenv("INIT_LANGUAGE", default = "zh") #amend here to change your preset language
+chat_language = os.getenv("INIT_LANGUAGE", default="zh") #altere aqui para mudar seu idioma predefinido
 	
 conversation = []
 
 class ChatGPT:  
-    
-
     def __init__(self):
-        
         self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-
-
+        self.model = os.getenv("OPENAI_MODEL", default="gpt-3.5-turbo")
 
     def get_response(self, user_input):
         conversation.append({"role": "user", "content": user_input})
-        
-
         response = openai.ChatCompletion.create(
-	            model=self.model,
-                messages = self.messages
-
-                )
-
+            model=self.model,
+            messages=self.messages
+        )
         conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-        
-        print("AI回答內容：")        
+        print("Conteúdo da resposta da IA:")
         print(response['choices'][0]['message']['content'].strip())
-
-
-        
         return response['choices'][0]['message']['content'].strip()
-	
-
-
-
-
-
-
 
 #####################
 
 telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
-
-
 
 # Load data from config.ini file
 #config = configparser.ConfigParser()
@@ -72,38 +48,29 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 # Initial bot by Telegram access token
-bot = telegram.Bot(token=telegram_bot_token)
-
+bot = telebot.TeleBot(telegram_bot_token)
 
 
 @app.route('/callback', methods=['POST'])
 def webhook_handler():
     """Set route /hook with POST method will trigger this method."""
     if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        update = telebot.types.Update.de_json(request.get_json(force=True), bot)
 
-        # Update dispatcher process that handler to process this message
-        dispatcher.process_update(update)
+        # Update bot with received update
+        bot.process_new_updates([update])
     return 'ok'
 
 
-def reply_handler(bot, update):
-    """Reply message."""
-    #text = update.message.text
-    #update.message.reply_text(text)
-    chatgpt = ChatGPT()        
-    
-                                            #update.message.text 人類的問題 the question humans asked
-    ai_reply_response = chatgpt.get_response(update.message.text) #ChatGPT產生的回答 the answers that ChatGPT gave
-    
-    update.message.reply_text(ai_reply_response) #用AI的文字回傳 reply the text that AI made
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Olá! Estou aqui para ajudar. Envie-me uma mensagem.")
 
-# New a dispatcher for bot
-dispatcher = Dispatcher(bot, None)
-
-# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# message.
-dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
+@bot.message_handler(func=lambda message: True)
+def reply_handler(message):
+    chatgpt = ChatGPT()
+    ai_reply_response = chatgpt.get_response(message.text)
+    bot.reply_to(message, ai_reply_response)
 
 if __name__ == "__main__":
     # Running server
